a44e5cd39aca30d0fe36cd3a87c597886bbbb49e
Prakhar Singh Rana <156541082+prakharsinghrana23@users.noreply.github.com>
2024-09-17 22:26:19 +0530

    Add files via upload

---
+++ day1.py
@@ -0,0 +1,50 @@
+#LEARNING PRINT FUNCTION 
+# Write your code below this
+print("Hello world")
+print("1. Mix 500g of Flour, 10g Yeast and 300ml Water in a bowl.")
+print("2. Knead the dough for 10 minutes.")
+print("3. Add 3g of Salt.")
+print("4. Leave to rise for 2 hours.")
+print("5. Bake at 200 degrees C for 30 minutes.")
+
+#string manipulation and code intelligence
+print("Prakhar Singh Rana\n Ojit Chauhan")
+#\n used for line gap 
+
+ #STRING MANIPULATION learn to gove a space by using \n function
+ #Write your code below this line 👇
+print("Hello world!\nHello World!\nHello World!")
+
+print("Hello " + "Prakhar")
+
+#learning to take inputs from user 
+#input("what is your name?")
+print("Hello" + input("what is your name?"))
+#this code will take the input function from the user by using
+
+#learning variables (length function)
+#from operator import length_hint
+
+name=input("What is your name?")
+print(name)
+
+name="Ojit Chauhan"
+print(name)
+
+print(len(input("what is your name")))
+
+username=input("What is your name?")
+length=len(username)
+print(length )
+
+
+#variable naming
+user_name = "Prakhar"
+length = len(user_name)
+print(length)
+
+#band name generator project
+print("Welcome to the band name generator")
+city = input("Which city did you grow up in??\n")
+pet = input("What is the name of your pet?\n")
+print("Your band name could be:" + city + " " + pet)
